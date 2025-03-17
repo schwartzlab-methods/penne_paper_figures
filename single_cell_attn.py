d3bbedf3bd9076bf95ec4897bc2e31838d987efd
YinniKun <dongrichard123@gmail.com>
2025-03-17 16:36:34 -0400

    add workflow for compute single-cell attention in pannuke

---
+++ single_cell_attn.py
@@ -0,0 +1,150 @@
+'''
+Generate the feature representation of all cells in the PanNuke dataset using the Phikon-v2 model.
+To get each cell, we treat the top 60% of the attention map as the most important regions.
+We then extract the features from the model for each of these regions by calculating an averged sum of the features using the attention map and embeddings.
+'''
+
+import torch
+from tqdm import tqdm
+from transformers import AutoImageProcessor, AutoModel
+from _feature_extractors import owkin_features
+from dataset import PanNukeDataset
+import numpy as np
+import os
+import torchvision.transforms.v2 as v2
+from skimage import measure
+import argparse
+
+def get_attn_features(extractor, processor, x):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # convert x to tensor
+    toimage = v2.ToImage().unsqueeze(0)
+    x = toimage(x)
+    # reshape the image to (3, 256, 256)
+    resizer = v2.Resize(256)
+    x = resizer(x)
+    # centre crop to (224, 224)
+    cropper = v2.CenterCrop(224)
+    x = cropper(x)
+    # x = torch.tensor(x).permute(2, 0, 1).unsqueeze(0)
+    # get attention and features
+    attention, features = owkin_features(extractor, device, processor, x, return_attn=True)
+    return x, attention, features
+
+def compute_single_cell_features(attn: torch.Tensor, img: torch.Tensor, fea: torch.Tensor, 
+                       size: int = 224, threshold: float = 0.6) -> list[tuple[np.ndarray, np.ndarray, np.ndarray]]:
+    '''
+    Process the attention values to generate a heatmap of size x size based on threshold
+    !!!! Only works if the batch size is 1 !!!!!
+    Shapes:
+    attn: (1, num_heads, num_patches, num_patches)
+    img: (1, 3, 224, 224)
+    fea: (1, num_patches+1, hidden_size)
+
+    Returns:
+    list of tuples, each tuple contains:
+    - cell_img: np.ndarray of size (size, size, 3)
+    - cell_mask: np.ndarray of size (size, size)
+    - cell_weighted_feature: np.ndarray of size (hidden_size)
+    '''
+    #! step1: generate the tresholded attention map
+    # get the attention except for the cls token
+    class_token_attention = attn[:, :, 0, 1:]
+    nh = class_token_attention.shape[1]
+    # we keep only a certain percentage of the mass according to threshold
+    val, idx = torch.sort(class_token_attention.reshape(nh, -1))
+    val /= torch.sum(val, dim=1, keepdim=True)
+    cumval = torch.cumsum(val, dim=1)
+    th_attn = cumval > (1 - threshold)
+    idx2 = torch.argsort(idx)
+    for head in range(class_token_attention.shape[1]):
+        th_attn[head] = th_attn[head][idx2[head]]
+    num_patches_side = int(class_token_attention.shape[-1] ** 0.5)
+    class_token_attention = th_attn.reshape(1, nh, num_patches_side, num_patches_side).float()
+    # average all the head
+    class_token_attention = class_token_attention.mean(dim=1) # (1, num_patches, num_patches)
+    attention_map = class_token_attention.view(num_patches_side, num_patches_side).cpu() # (num_patches, num_patches)
+
+    #! step2: segment the cells using thresholding
+    # change the attention map to binary
+    attention_map_binary = (attention_map > 0).float()
+    # get the connected components
+    labels = measure.label(attention_map_binary.numpy())
+    
+    #! step3: get the CLS attention for each cell
+    reshaped_features = fea[:, 1:] # (num_patches, hidden_size)
+    np_attention_map = attention_map.cpu().numpy() # (num_patches, num_patches) 
+    results = []
+    for cell in range(1, labels.max() + 1):
+        cell_mask: np.ndarray = (labels == cell).astype(np.float32) # (num_patches**0.5, num_patches**0.5)
+        cell_attn: np.ndarray = np_attention_map * cell_mask # (num_patches**0.5, num_patches**0.5)
+        cell_attn = cell_attn.reshape(-1) # (num_patches)
+        cell_attn_tensor = torch.tensor(cell_attn)
+        cell_features = reshaped_features * cell_attn_tensor.unsqueeze(-1) # (num_patches, hidden_size)
+        cell_weighted_feature = cell_features.sum(dim=0) # (hidden_size)
+        # extrapolate masks to size x size, with only 1 and 0
+        resized_mask: torch.Tensor = torch.nn.functional.interpolate(torch.tensor(cell_mask).unsqueeze(0).unsqueeze(0).float(), 
+                                                       size=(size, size), mode='nearest')
+        # convert the resized_mask to binary
+        resized_mask = (resized_mask > 0)
+        # get the image of the cell
+        cell_img: torch.Tensor = img[resized_mask]
+        results.append((cell_img.cpu().numpy().squeeze(0).transpose(1, 2, 0),
+                        resized_mask.squeeze(0).cpu().numpy().transpose(1, 2, 0),
+                        cell_weighted_feature.cpu().numpy()))
+
+    return results
+
+def determine_cell_type(cell_mask: np.ndarray, img_mask: np.ndarray) -> tuple[str, float]:
+    '''
+    Determine the cell type based on the mask and the label, obtain the most frequent label in the mask
+    cell_mask: np.ndarray of size (224, 224, 1)
+    img_mask: np.ndarray of size (224, 224, 6) in one-hot encoding
+    Also provide the percent frequencies for the label
+    '''
+    label_dic = {
+        0: "Neoplastic", 1: "Inflammatory", 2: "Connective/Soft_tissue", 3: "Dead", 4: "Epithelial", 6: "Background"
+    }
+    roi = img_mask[cell_mask]
+    roi_non_onehot: np.ndarray = np.argmax(roi, axis=-1)
+    roi_label: int = np.argmax(np.bincount(roi_non_onehot.flatten()))
+    roi_label_freq = np.bincount(roi_non_onehot.flatten())[roi_label] / roi_non_onehot.size
+    return (label_dic[roi_label], roi_label_freq)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root_dir', type=str, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+    args = parser.parse_args()
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    data = PanNukeDataset(args.root_dir)
+    feature_extractor = AutoModel.from_pretrained("/fs01/home/richarddong/.cache/huggingface/hub/phikon-v2")
+    image_processor = AutoImageProcessor.from_pretrained("owkin/phikon-v2")
+    cell_type = []
+    cell_feature = []
+    cell_type_freq = []
+    tissue_type = []
+    for each in tqdm(data):
+        img, attn, fea = get_attn_features(feature_extractor, image_processor, each[0])
+        cell_img, cell_mask, cell_feature = compute_single_cell_features(attn, img, fea)
+        # center crop the label to (224,224)
+        img_mask = each[1]
+        cropped_mask = v2.CenterCrop(224)(torch.tensor(img_mask).unsqueeze(0)).squeeze(0).numpy()
+        for idx, _ in enumerate(cell_img):
+            cell_type, cell_freq = determine_cell_type(cell_mask[idx], cropped_mask)
+            cell_type.append(cell_type)
+            cell_type_freq.append(cell_freq)
+            cell_feature.append(cell_feature[idx])
+            tissue_type.append(each[2])
+    cell_type = np.array(cell_type)
+    cell_type_freq = np.array(cell_type_freq)
+    cell_feature = np.array(cell_feature)
+    tissue_type = np.array(tissue_type)
+    np.save(os.path.join(args.output_dir, "cell_type.npy"), cell_type)
+    np.save(os.path.join(args.output_dir, "cell_type_freq.npy"), cell_type_freq)
+    np.save(os.path.join(args.output_dir, "cell_feature.npy"), cell_feature)
+    np.save(os.path.join(args.output_dir, "tissue_type.npy"), tissue_type)
+
+if __name__ == '__main__':
+    main()
