499e0c5e9c2d4a35b16f6eac8406674f879d5ce9
YinniKun <dongrichard123@gmail.com>
2025-08-12 00:39:05 -0400

    multiprocessing instead

--- visiumdhd_preprocessing/generate_patch_mtx_visium.py
+++ visiumdhd_preprocessing/generate_patch_mtx_visium.py
@@ -9,15 +9,13 @@ import numpy as np
 import pandas as pd
 import argparse
 import scanpy as sc
-import anndata as ad
 import concurrent.futures
 import tifffile
 from PIL import Image
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
-def process(img: np.array, position_matrix: pd.DataFrame, 
-            cell_matrix: ad.AnnData, name: str, each_barcode: str,
-            out: dir) -> None:
+def process(img_path: str, position_matrix: pd.DataFrame, cell_matrix: sc.AnnData, name: str, each_barcode: str, out: str) -> None:
     '''Process a single image patch and its corresponding spatial and expression data.
 
     Args:
@@ -28,26 +26,24 @@ def process(img: np.array, position_matrix: pd.DataFrame,
         each_barcode (str): The barcode corresponding to the current patch.
         out (dir): The output directory.
     '''
-    try:
-        img_width = img.shape[1]
-        img_height = img.shape[0]
-        x = position_matrix.loc[position_matrix.barcode == each_barcode, 'pxl_col_in_fullres'].values[0]
-        y = position_matrix.loc[position_matrix.barcode == each_barcode, 'pxl_row_in_fullres'].values[0]
-        mtx = cell_matrix[cell_matrix.obs_names == each_barcode].X
-        # check if save is needed
-        if x < 112 or y < 112 or x + 112 > img_width or y + 112 > img_height:
-            return None  # Skip if the patch is out of bounds
-        if mtx.shape[0] == 0:
-            return None  # Skip if there are no barcode found
-        if mtx.sum() == 0:
-            return None  # Skip if there are no expression values found
-        # process image
-        cropped_image = img[y - 112:y + 112, x - 112:x + 112]
-        Image.fromarray(cropped_image).save(os.path.join(out, "tissue_img", f"{name}_{each_barcode}.png"))
-        # process exp
-        np.save(os.path.join(out, "mtx", f"{name}_{each_barcode}.npy"), mtx)
-    except Exception as e:
-        print(e)
+    img = tifffile.imread(img_path)
+    img_width = img.shape[1]
+    img_height = img.shape[0]
+    x = position_matrix.loc[position_matrix.barcode == each_barcode, 'pxl_col_in_fullres'].values[0]
+    y = position_matrix.loc[position_matrix.barcode == each_barcode, 'pxl_row_in_fullres'].values[0]
+    mtx = cell_matrix[cell_matrix.obs_names == each_barcode].X
+    # check if save is needed
+    if x < 112 or y < 112 or x + 112 > img_width or y + 112 > img_height:
+        return None  # Skip if the patch is out of bounds
+    if mtx.shape[0] == 0:
+        return None  # Skip if there are no barcode found
+    if mtx.sum() == 0:
+        return None  # Skip if there are no expression values found
+    # process image
+    cropped_image = img[y - 112:y + 112, x - 112:x + 112]
+    Image.fromarray(cropped_image).save(os.path.join(out, "tissue_img", f"{name}_{each_barcode}.png"))
+    # process exp
+    np.save(os.path.join(out, "mtx", f"{name}_{each_barcode}.npy"), mtx)
 
 def find_common_genes(cell_matrices: list, out: str) -> list:
     '''Find common genes across all cell matrices.
@@ -106,6 +102,11 @@ def main(all_dir: str, output: str, common_genes: str) -> None:
             common_genes_list = [line.strip() for line in f.readlines()]
     else:
         common_genes_list = find_common_genes(cell_mtx_list, output)
+    if common_genes:
+        with open(common_genes, "r") as f:
+            common_genes_list = [line.strip() for line in f.readlines()]
+    else:
+        common_genes_list = find_common_genes(cell_mtx_list, output)
     print("Common genes number: ", len(common_genes_list))
     # filter the cell matrices to only contain common genes
     for i in range(len(cell_mtx_list)):
@@ -113,16 +114,15 @@ def main(all_dir: str, output: str, common_genes: str) -> None:
         # sort according to the common genes
         cell_mtx_list[i].var_names = cell_mtx_list[i].var_names.astype(str)
         cell_mtx_list[i] = cell_mtx_list[i][:,cell_mtx_list[i].var.sort_values("gene_ids").index].copy()
-    # process by saving the cropped image and mtx according to the coordinates
-    print("Files loaded and matrix normalized. Start processing")
     total_tasks = sum(len(pos_mtx.barcode) for pos_mtx in pos_mtx_list)
-    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
-        futures = []
-        for i in tqdm(range(len(img_list))):
-            img = tifffile.imread(img_list[i])
+    futures = []
+    print(f"start with {os.cpu_count()} workers")
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        for i in range(len(img_list)):
+            img_path = img_list[i]
             name = os.path.basename(os.path.dirname(img_list[i]))
             for each_barcode in pos_mtx_list[i].barcode:
-                futures.append(executor.submit(process, img, pos_mtx_list[i], cell_mtx_list[i], name, each_barcode, output))
+                futures.append(executor.submit(process, img_path, pos_mtx_list[i], cell_mtx_list[i], name, each_barcode, output))
         for _ in tqdm(concurrent.futures.as_completed(futures), total=total_tasks, desc="Processing patches"):
             pass
     print("All images have been processed")
