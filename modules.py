1f583b9b2ae83cd487842e2ad28449621eae9e63
YinniKun <dongrichard123@gmail.com>
2025-08-19 15:55:07 -0400

    make the translator a bit more complex

--- modules.py
+++ modules.py
@@ -18,12 +18,16 @@ class Translator(nn.Module):
     def __init__(self, feature_dim=1024, hidden_dim=512, output_dim=1024):
         super(Translator, self).__init__()
         self.fc1 = nn.Linear(feature_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, output_dim) 
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim) 
+        self.dropout = nn.Dropout(0.3)
         self.bn = nn.BatchNorm1d(hidden_dim)
     
     def forward(self, x):
         x = F.relu(self.bn(self.fc1(x)))
-        return self.fc2(x)  # No activation to keep range flexible
+        x = self.dropout(x)
+        x = F.relu(self.bn(self.fc2(x)))
+        return self.fc3(x)  # No activation to keep range flexible
 
 #* Gradient reversal layer (for adversarial training)
 class GradReverse(torch.autograd.Function):
